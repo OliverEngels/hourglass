1f818ac348befd53a57b6e20ba82a387dfe53bbd
Oliver Engels <contact@oliverengels.com>
2024-05-25 17:20:38 +0200

    feat(api): add put for entry

--- pages/api/entry/[id].ts
+++ pages/api/entry/[id].ts
@@ -7,35 +7,60 @@ import CORS from '../middleware';
 export default async function handler(req: NextApiRequest, res: NextApiResponse<ApiResponse>) {
     await CORS(req, res);
 
-    if (req.method === 'DELETE') {
-        const { id } = req.query;
+    const { id, ...rest } = req.body;
+    switch (req.method) {
+        case 'DELETE':
+            try {
+                const client = await clientPromise;
+                const db = client.db('hourglass');
+                const collection = db.collection('entries');
 
-        try {
-            const client = await clientPromise;
-            const db = client.db('hourglass');
-            const collection = db.collection('entries');
+                if (Array.isArray(id)) {
+                    return res.status(400).json({
+                        success: false,
+                        message: 'Invalid ID format'
+                    });
+                }
 
-            if (Array.isArray(id)) {
-                return res.status(400).json({
+                const deleteResult = await collection.deleteOne({ _id: new ObjectId(id) });
+
+                res.status(201).json({
+                    success: true,
+                    message: 'Entry successfully deleted'
+                });
+            } catch (e) {
+                res.status(500).json({
                     success: false,
-                    message: 'Invalid ID format'
+                    message: `Failed to insert entry: ${e.message}`
                 });
             }
+            break;
+        case 'PUT':
+            try {
+                const client = await clientPromise;
+                const db = client.db('hourglass');
+                const collection = db.collection('entries');
 
-            const deleteResult = await collection.deleteOne({ _id: new ObjectId(id) });
+                const filter = { _id: new ObjectId(id) };
+                const updateDoc = {
+                    $set: rest,
+                };
+                const result = await collection.updateOne(
+                    filter, updateDoc, { upsert: true });
 
-            res.status(201).json({
-                success: true,
-                message: 'Entry successfully deleted'
-            });
-        } catch (e) {
-            res.status(500).json({
-                success: false,
-                message: `Failed to insert entry: ${e.message}`
-            });
-        }
-    } else {
-        res.setHeader('Allow', ['DELETE']);
-        res.status(405).end(`Method ${req.method} Not Allowed`);
+                res.status(201).json({
+                    success: true,
+                    message: 'Entry successfully updated'
+                });
+            } catch (e) {
+                res.status(500).json({
+                    success: false,
+                    message: `Failed to insert entry: ${e.message}`
+                });
+            }
+            break;
+        default:
+            res.setHeader('Allow', ['DELETE', 'PUT']);
+            res.status(405).json({ success: false, message: 'Method Not Allowed' });
     }
 }
