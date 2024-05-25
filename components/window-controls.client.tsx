3ee75757f4ca9457206d59b9fc39867aa4ca0f47
Oliver Engels <contact@oliverengels.com>
2024-05-25 15:44:46 +0200

    feat(screen): make header region draggable

--- components/window-controls.client.tsx
+++ components/window-controls.client.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const WindowControls = () => {
     return (
-        <div className='fixed top-0 left-0 w-full h-15 z-30'>
+        <div className='fixed top-0 left-0 w-full h-15 z-30 app-region-drag'>
             <div className="flex space-x-2 p-4">
-                <button onClick={() => window.electron.closeWindow()} className="bg-red-500 w-3 h-3 rounded-full hover:bg-red-700 z-10"></button>
-                <button onClick={() => window.electron.minimize()} className="bg-yellow-500 w-3 h-3 rounded-full hover:bg-yellow-700 z-10"></button>
-                <button onClick={() => window.electron.maximize()} className="bg-green-500 w-3 h-3 rounded-full hover:bg-green-700 z-10"></button>
+                <button onClick={() => window.electron.closeWindow()} className="bg-red-500 w-3 h-3 rounded-full hover:bg-red-700 z-10 app-region-no-drag"></button>
+                <button onClick={() => window.electron.minimize()} className="bg-yellow-500 w-3 h-3 rounded-full hover:bg-yellow-700 z-10 app-region-no-drag"></button>
+                <button onClick={() => window.electron.maximize()} className="bg-green-500 w-3 h-3 rounded-full hover:bg-green-700 z-10 app-region-no-drag"></button>
             </div>
             <div className="w-full h-10 fixed left-0 top-0 backdrop-blur-md bg-cover bg-center" />
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,58 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    './styles/**/*.css'
-  ],
-  theme: {
-    extend: {
-      colors: {
-        "tag-color-red": '#FF0000',
-        "tag-color-green": '#00FF00',
-        "tag-color.blue": '#0000FF',
-      },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        './styles/**/*.css'
+    ],
+    theme: {
+        extend: {
+            colors: {
+                "tag-color-red": '#FF0000',
+                "tag-color-green": '#00FF00',
+                "tag-color.blue": '#0000FF',
+            },
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic":
+                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+            addUtilities: {
+                '.app-region-drag': {
+                    '-webkit-app-region': 'drag',
+                },
+                '.app-region-no-drag': {
+                    '-webkit-app-region': 'no-drag',
+                },
+            },
+        },
     },
-  },
-  variants: {
-    extend: {
-      textColor: ['responsive', 'hover', 'focus', 'peer-focus', 'peer-hover'],
+    variants: {
+        extend: {
+            textColor: ['responsive', 'hover', 'focus', 'peer-focus', 'peer-hover'],
+        },
     },
-  },
-  safelist: [
-    'bg-red-50', 'bg-blue-50', 'bg-green-50', 'bg-yellow-50', 'bg-purple-50', 'bg-orange-50', 'bg-pink-50', 'bg-white',
-    'ring-red-500/10', 'ring-blue-500/10', 'ring-green-500/10', 'ring-yellow-500/10', 'ring-purple-500/10', 'ring-orange-500/10', 'ring-pink-500/10',
-    'bg-red-100', 'bg-blue-100', 'bg-green-100', 'bg-yellow-100', 'bg-purple-100', 'bg-orange-100', 'bg-pink-100',
-    {
-      pattern: /(bg|border|ring|text)-(red|green|blue|yellow|pink|purple|orange|gray|lime)-[1-7](00|0)/,
-      variants: ['hover', 'focus'],
-    }
-  ],
-  plugins: [],
+    safelist: [
+        'bg-red-50', 'bg-blue-50', 'bg-green-50', 'bg-yellow-50', 'bg-purple-50', 'bg-orange-50', 'bg-pink-50', 'bg-white',
+        'ring-red-500/10', 'ring-blue-500/10', 'ring-green-500/10', 'ring-yellow-500/10', 'ring-purple-500/10', 'ring-orange-500/10', 'ring-pink-500/10',
+        'bg-red-100', 'bg-blue-100', 'bg-green-100', 'bg-yellow-100', 'bg-purple-100', 'bg-orange-100', 'bg-pink-100',
+        {
+            pattern: /(bg|border|ring|text)-(red|green|blue|yellow|pink|purple|orange|gray|lime)-[1-7](00|0)/,
+            variants: ['hover', 'focus'],
+        }
+    ],
+    plugins: [
+        function ({ addUtilities }) {
+            const newUtilities = {
+                '.app-region-drag': {
+                    '-webkit-app-region': 'drag',
+                },
+                '.app-region-no-drag': {
+                    '-webkit-app-region': 'no-drag',
+                },
+            }
+
+            addUtilities(newUtilities, ['responsive', 'hover'])
+        }
+    ],
 };
